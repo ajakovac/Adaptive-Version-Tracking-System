20e21d61ec85b4d75973472b2d14eaed7df20848
Antal Jakovác <antal.jakovac@gmail.com>
2020-02-27 07:02:09 +0100

    New version: boolean decision making

--- AVTS.py
+++ AVTS.py
@@ -29,38 +29,87 @@ import sys
 import re
 import os.path
 
-remark = "#"
-AVTSpattern = remark+r"?\s*AVTS\s*{([^}]+)}"
+AVTSid = r"(#|//)?\s*AVTS\s*([a-zA-Z]+)\s*({([^}]+)})?\s*$"
+commands = ["begin", "end"]
+# parses line to find AVTS directives
+# returns False, None, None if it is a normal line
+# returns True, commandtoken, id if it is AVTS line
 def AVTSparse(line) :
-    m = re.match(AVTSpattern, line)
+    m = re.match(AVTSid, line)
     if (not m) :
-        return False, "", 0
-    id = m.group(1)
-    idlist = id.split(":")
-    if (len(idlist) == 1) :
-        level = 0
-    elif (len(idlist) == 2) :
-        id = idlist[0]
-        level = int(idlist[1])
-    else :
-        print("corrupted AVTS ID in: " + line)
-        exit(-3)
-    return True, id, level
+        return False, None, None
+    cmnd = m.group(2)
+    if cmnd not in commands :
+        print('bad command in ', line)
+    return True, cmnd, m.group(4)
 
-sys.argv.pop(0)
+# collects variables in a boolean expression
+def checkVariables(line) :
+    return list(filter(lambda x : not re.match(r'^or|and$',x) and len(x)>0, 
+                re.split(r"\W+",line)))
+
+sys.argv.pop(0)  # the filename
 if (len(sys.argv) == 0 ) :
-    print('Not enough arguments: base file is missing')
+    print('Not enough arguments')
     exit(-1)
 
+# handles switches in the input line
 save_master = False
+optionhash = {}
+masterfile = None
+
 while (sys.argv[0])[0] == '-' :
     opt = sys.argv.pop(0)
     if (opt == '-help') :
         print(helptext)
         exit(0)
-    if (opt == '-save_update') :
+    if opt == '-save_master' :
         save_master = True
+        continue
+    m = re.match(r"-f", opt)
+    if m :  # found a master file
+        masterfile = sys.argv.pop(0)
+        print("masterfile = ", masterfile, file = sys.stderr)
+        try :
+            f = open(masterfile)
+            mastertext = f.read().splitlines()
+            f.close()
+        except IOError as err :
+            print("IOError in master file: {0}".format(err))
+        # create the option hash file from the master file
+        IDpattern = r"\s*([^=\s]+)\s*=\s*(True|False)"
+        for line in mastertext :
+            if(len(line) == 0) : continue  # empty line
+            if (line[0] == '#') : continue  # remark
+            m = re.match(IDpattern, line)
+            if(not m) :
+                print("Bad format in master file : ", line)
+            exec(m.group(0))  # we treat each line as a python command!
+            if m.group(1) in optionhash :
+                optionhash[m.group(1)] = bool(m.group(2))
+            else :
+                optionhash.update({m.group(1) : bool(m.group(2))})
+        continue
+    m = re.match(r"-T(\w+)", opt)
+    if m :  # found a variable to set true
+        exec(m.group(1) + "=True")
+        if m.group(1) in optionhash :
+            optionhash[m.group(1)] = True
+        else :
+            optionhash.update({m.group(1) : True})
+        continue
+    m = re.match(r"-F(\w+)", opt)
+    if m :  # found a variable to set false
+        exec(m.group(1) + "=False")
+        if m.group(1) in optionhash :
+            optionhash[m.group(1)] = False
+        else :
+            optionhash.update({m.group(1) : False})
+        continue
+    print("Bad switch : ", opt, file = sys.stderr)
+    exit(-1)
 
+# end of switches: input file must come
 inputfile = sys.argv.pop(0)
 try :
     f = open(inputfile)
@@ -70,67 +119,44 @@ except IOError as err:
     print("IOError in input file: {0}".format(err))
     exit(-1)
 
-m = re.match(r"(.*)\.([^\.]*)", inputfile)
-if m :  
-    base = m.group(1)
-    extension = m.group(2)
-else :  
-    base = inputfile
-    extension = ""
-
-AVTSmasterfile = None
-if (len(sys.argv) == 0) :  # non explicit master file
-    tryfile = base + "_AVTSmaster"
-    if (os.path.isfile(tryfile)) :
-        AVTSmasterfile = tryfile
-else :
-    AVTSmasterfile = sys.argv.pop(0)
-
-if (AVTSmasterfile is not None) :
-    try :
-        f = open(AVTSmasterfile)
-        mastertext = f.read().splitlines()
-        f.close()
-    except IOError as err :
-        print("IOError in master file: {0}".format(err))
-else :
-    print("WARNING: No available AVTS master file found.", file=sys.stderr)
-    print("To save changes use -save_update switch!", file=sys.stderr)
-    AVTSmasterfile = base + "_AVTSmaster"
-    mastertext = []
-
-# create the option hash file from the master file
-optionhash = {}
-for line in mastertext :
-    if(len(line) == 0) : continue  # empty line
-    if (line[0] == '#') : continue  # remark
-    llist = line.split(':')
-    print(llist)
-    optionhash.update({llist[0] : int(llist[1])})
-
 # create option status
-statushash = optionhash.copy()
-for k in statushash :
-    statushash[k] = False  # neither of the key were used
-
-# process the text according to the optionhash
+statusstack = [True]
 visible = True  # we start with a visible status
 
+# process the text according to the optionhash
 for line in basetext :
-    isAVTS, id, level = AVTSparse(line)
-    if (not isAVTS and visible) :
-        print(line)
+    isAVTS, cmnid, boolexpr = AVTSparse(line)
+    if (not isAVTS):
+        if visible : print(line)
         continue
-    idvalue = optionhash.get(id)
-    if(idvalue is None) :  # tag is not in the master file
-        optionhash.update({ id : -1 })  # restrictive mode
-        statushash.update({ id : False })
-        idvalue = -1
-    if(statushash[id]) :
-        statushash[id] = False
-    else :  
-        statushash[id] = True
-    if(level != idvalue) :
-        if(statushash[id]) : visible = False
-        else : visible = True
+    if boolexpr is not None :
+        varlist = checkVariables(boolexpr)
+        for var in varlist :
+            if var not in optionhash :
+                optionhash.update({ var : False})
+                exec( var + "= False")
+            booleval = eval(boolexpr)
+    else :  booleval = None
+    if cmnid == "end" :
+        visible = statusstack.pop()
+        continue
+    if cmnid == "begin" :
+        if booleval is None :
+            print("bad syntax in ", line)
+            exit(-1)
+        statusstack.append(visible)
+        visible = booleval
+        
+
+# save master file if necessary
+if save_master :
+    if masterfile is None :
+        m = re.match(r"(.*)\.[^.]+", inputfile)
+        if m : masterfile = m.group(1) + "_master"
+        else : masterfile = inputfile + "_master"
+    f = open(masterfile, 'w')
+    print("# AVTS master, automatic save", file = f)
+    for i in optionhash :
+        if optionhash[i] : print( i + " = True", file = f)
+        else : print( i + " = False", file = f)
  
