1d95675549038597a537d54b9bb7bd31cbd26c45
Antal Jakovác <antal.jakovac@gmail.com>
2020-03-10 11:17:55 +0100

    redefined avts commands

--- AVTS.py
+++ AVTS.py
@@ -5,48 +5,168 @@
 helptext = """
 Adaptive Version Tracking System (AVTS):
 this system is a filter with higly tunable filtering rules.
-The code works with two files, an input file and a master file. 
-The input file can be any text file (including documentations,
-program codes, etc.) containing AVTS switches, which define and tag 
-blocks of the input file. The master file determines whether these blocks
-appear in the output or not. For more information refer the documentation.
+
+The logics of the filter is that the lines of the input file is printed
+according to the current environment. The environment depends on the 
+application forum (eg. txt, pdf, html, etc.), and it has a lot of tunable
+parameters (for example visibility, text style, color, position, action
+on clicks, etc.). The environment can be changed by different 
+context functions which maps an environment to an environment, and my 
+depend on external user defined variables. The application of the
+context functions as well as the definition and use of the user defined
+varaibles can be changed by the AVTS directives.
+
+The AVTS directives are contained by the input file. All directives are
+introduced by a remark sign (that can be #, % or //), followed by
+the AVTS keyword, then a command and the arguments in curly brackets.
+Currently the available directives are:
+
+# AVTS def <variable> [= <value>] ["<description>"]
+defines a variable, optionally assign an initial value to it, and give
+a description. The user defined variables are unmutable, and their
+value comes from the FIRST occurance. The reason behind this rule is that
+the variables are first defined in the command line and later in the
+input file. Predefined variable is "current" which is used as the
+visibility variable of the current environment.
+
+# AVTS context <visibility bool>, <style description>
+modifies the actual context. The visibility bool is a Python expression,
+which determines the visibility. The current visibility can be achived 
+under the variable "current". The style description, which is a list
+of context functions, will act on the basic environment one after another,
+determining the current environment. The old context is pushed into a stack.
+All the variables occuring in the expressions or style descriptions 
+must be defined earlier.
+
+# AVTS recover
+restores the last environment from the stack
+
+# AVTS reset
+resets the environment stack, and takes the basic environment.
+
+The defined user variables can be set in the command line as well as
+in a master file. 
 
 Usage:
-python3 AVTS.py <swiches> inputfile <master file>
+AVTS.py <swiches> inputfile
 
 available switches:
 -help : prints this helptext
--save_update : if there are tags in the input file that are missing
-        from the master file, updates the master file accordingly
-        The new items are as default unvisible
+- f masterfile: reads in the definitions of the variables from 
+the masterfile. In this case the AVTS define directive may be omitted,
+if these are shown up already in the master file
+-save_update : the defined variables are saved in the master file. If 
+no master file is given, we use the core input file name extended by
+"_AVTSmaster"
+-D<variable=value> : define a variable, and set its value
 
-The input file is compulsory. The default for the master file is
-the input file core (without extension) followed by "_AVTSmaster",
-and no extension.
 """
 
 import sys
 import re
 import os.path
+import copy
 
-AVTSid = r"(#|//)?\s*AVTS\s*([a-zA-Z]+)\s*({([^}]+)})?\s*$"
-commands = ["begin", "end"]
-# parses line to find AVTS directives
-# returns False, None, None if it is a normal line
-# returns True, commandtoken, id if it is AVTS line
-def AVTSparse(line) :
-    m = re.match(AVTSid, line)
-    if (not m) :
-        return False, None, None
-    cmnd = m.group(2)
-    if cmnd not in commands :
-        print('bad command in ', line)
-    return True, cmnd, m.group(4)
+# cf. https://en.wikipedia.org/wiki/ANSI_escape_code
+# these are the environment modifying contexts
+def setcolor(r,g,b) :
+    return u"\033[38;2;" + str(r) + ";" + str(g) + ";" + str(b) + "m"
 
-# collects variables in a boolean expression
-def checkVariables(line) :
-    return list(filter(lambda x : not re.match(r'^or|and$',x) and len(x)>0, 
-                re.split(r"\W+",line)))
+def setbgcolor(r,g,b) :
+    return u"\033[48;2;" + str(r) + ";" + str(g) + ";" + str(b) + "m"
+
+class ANSIcode :
+    reset = u"\033[m"
+    bold = u"\033[1m"
+    italic = u"\033[3m"
+    underline = u"\033[4m"
+    clear = u"\033[2J"
+
+# the current lines are printed according to the actual Environment
+class Environment :
+    visible = True
+    indent = ""
+    modline = ""
+    color = setcolor(0,0,0)
+    def str(self) :
+        return(self.modline)
+    def print(self, line) :
+        if self.visible : 
+            print(self.indent + self.modline + self.color + line + ANSIcode.reset, end='')
+
+def mainTextcommand(env_in) :
+    return Environment()
+
+mainText = lambda env : mainTextcommand(env)
+
+def remarkTextcommand(env_in) :
+    env = copy.copy(env_in)
+    env.modline = ANSIcode.italic
+    env.color = setcolor(120,120,120)
+    env.indent = "   "
+    return env
+
+remarkText = lambda env : remarkTextcommand(env)
+
+def redTextcommand(env_in) :
+    env = copy.copy(env_in)
+    env.color = setcolor(255,0,0)
+    return env
+
+redText = lambda env : redTextcommand(env)
+
+
+# here starts the AVTS command parser
+dict ={"current" : "gives the visibility of the current environment"}
+current =True
+basicEnvironment = Environment()
+current = basicEnvironment.visible  # the current environment
+stack = [basicEnvironment]
+
+# interprets the current line as AVTS command.
+# if it is not AVTS command, returns False
+def AVTSinterpreter(line) :
+    global stack
+    AVTSdef = r'(#|//)?\s*AVTS\s+def\s+((\w+)\s*=\s*([\w\s]+))(\s+"([^"]*)\s*")?$'
+    m = re.match(AVTSdef, line)
+    if m : # it is a definition
+        if m.group(3) not in dict :
+            dict.update( { m.group(3) : m.group(6)})
+            return m.group(2)
+        else :
+            dict[m.group(3)] = m.group(6)
+            return ""
+    AVTScontext = r'(#|//)?\s*AVTS\s+context\s+([^,]+)((,[^,]+)*)$'
+    m = re.match(AVTScontext, line)
+    if m : # context change command
+        env = copy.copy(stack[-1])  # current environment is the last in the stack
+        commands = m.group(3).split(',')[1:] # read out contexts
+        commands.reverse()
+        for c in commands :
+            env = eval(c)(env)
+        env.visible = eval(m.group(2))
+        current = env.visible
+        stack.append(env)
+        return ""
+    AVTSrecover = r'(#|//)?\s*AVTS\s+recover\s*$'
+    m = re.match(AVTSrecover, line)
+    if m :
+        stack.pop()
+        env = stack[-1]
+        current = env.visible
+        return ""
+    AVTSreset = r'(#|//)?\s*AVTS\s+reset\s*$'
+    m = re.match(AVTSreset, line)
+    if m :
+        env = stack[0]
+        stack = [basicEnvironment]
+        current = env.visible
+        return ""
+    # not AVTS command: to be printed
+    env = stack[-1]
+    env.print(line)
+    return ""
+
 
 sys.argv.pop(0)  # the filename
 if (len(sys.argv) == 0 ) :
@@ -55,8 +175,8 @@ if (len(sys.argv) == 0 ) :
 
 # handles switches in the input line
 save_master = False
-optionhash = {}
 masterfile = None
+toexec = ""
 
 while (sys.argv[0])[0] == '-' :
     opt = sys.argv.pop(0)
@@ -77,76 +197,42 @@ while (sys.argv[0])[0] == '-' :
         except IOError as err :
             print("IOError in master file: {0}".format(err))
         # create the option hash file from the master file
-        IDpattern = r"\s*([^=\s]+)\s*=\s*(True|False)"
+        IDpattern = r'\s*((\w+)\s*=\s*([^"]+))(\s+"([^"]*)")?$'
         for line in mastertext :
             if(len(line) == 0) : continue  # empty line
             if (line[0] == '#') : continue  # remark
             m = re.match(IDpattern, line)
             if(not m) :
                 print("Bad format in master file : ", line)
-            exec(m.group(0))  # we treat each line as a python command!
-            if m.group(1) in optionhash :
-                optionhash[m.group(1)] = bool(m.group(2))
+            toexec = toexec + m.group(1) +"\n"  # we treat each line as a python command!
+            if m.group(2) not in dict :
+                dict.update({m.group(2) : m.group(5) })
             else :
-                optionhash.update({m.group(1) : bool(m.group(2))})
+                dict[m.group(2)] = m.group(5)
         continue
-    m = re.match(r"-T(\w+)", opt)
-    if m :  # found a variable to set true
-        exec(m.group(1) + "=True")
-        if m.group(1) in optionhash :
-            optionhash[m.group(1)] = True
-        else :
-            optionhash.update({m.group(1) : True})
-        continue
-    m = re.match(r"-F(\w+)", opt)
-    if m :  # found a variable to set false
-        exec(m.group(1) + "=False")
-        if m.group(1) in optionhash :
-            optionhash[m.group(1)] = False
-        else :
-            optionhash.update({m.group(1) : False})
+    m = re.match(r"-D", opt)
+    if m :  # found a variable to be defined
+        opt = sys.argv.pop(0)
+        m = re.match(r"((\w+)=(\w+))", opt)
+        toexec = toexec + m.group(1) +"\n"  # we treat each line as a python command!
+        if m.group(2) not in dict :
+            dict.update({m.group(2) : "command line defined variable"})
         continue
     print("Bad switch : ", opt, file = sys.stderr)
     exit(-1)
+
+exec(toexec)
 
 # end of switches: input file must come
 inputfile = sys.argv.pop(0)
 try :
     f = open(inputfile)
-    basetext = f.read().splitlines()
+    for line in f :
+        exec(AVTSinterpreter(line))
     f.close()
 except IOError as err:
     print("IOError in input file: {0}".format(err))
     exit(-1)
-
-# create option status
-statusstack = [True]
-visible = True  # we start with a visible status
-
-# process the text according to the optionhash
-for line in basetext :
-    isAVTS, cmnid, boolexpr = AVTSparse(line)
-    if (not isAVTS):
-        if visible : print(line)
-        continue
-    if boolexpr is not None :
-        varlist = checkVariables(boolexpr)
-        for var in varlist :
-            if var not in optionhash :
-                optionhash.update({ var : False})
-                exec( var + "= False")
-            booleval = eval(boolexpr)
-    else :  booleval = None
-    if cmnid == "end" :
-        visible = statusstack.pop()
-        continue
-    if cmnid == "begin" :
-        if booleval is None :
-            print("bad syntax in ", line)
-            exit(-1)
-        statusstack.append(visible)
-        visible = booleval
-        
 
 # save master file if necessary
 if save_master :
@@ -156,7 +242,6 @@ if save_master :
         else : masterfile = inputfile + "_master"
     f = open(masterfile, 'w')
     print("# AVTS master, automatic save", file = f)
-    for i in optionhash :
-        if optionhash[i] : print( i + " = True", file = f)
-        else : print( i + " = False", file = f)
- 
+    for i in dict :
+        print(i +"="+ str(eval(i)) +' "' + dict[i] +'"', file = f)
+    f.close()
